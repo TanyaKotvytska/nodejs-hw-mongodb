f5b346dc6f3d922d40c42f413398bd49829150c4
Tanya Kotvytska <tanyakotvitskaya20@gmail.com>
2024-10-23 10:30:16 +0300

    update

--- src/server.js
+++ src/server.js
@@ -4,58 +4,59 @@ import cors from 'cors';
 import { env } from './utils/env.js';
 import { getAllContacts, getContactById } from "./services/contacts.js";
 
-const logger = pino({
-	transport: {
-		target: 'pino-pretty',
-		options: { colorize: true }
-	}
-});
+const PORT = Number(env('PORT', '3000'));
+console.log(PORT);
 
 export const startServer = () => {
   const app = express();
+  app.use(express.json());
+
   app.use(cors());
-  app.use(logger);
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-  app.get("/contacts", async (req, res) => {
-    const data = await getAllContacts();
-    res.json({
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
       status: 200,
-      message: "Successfully found contacts!",
-      data
+      message: 'Successfully found contacts!',
+      data: contacts,
     });
   });
 
-  app.get("/contacts/:contactId", async (req, res) => {
+  app.get('/contacts/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
+    const contact = await getContactById(contactId);
 
-    const data = await getContactById(contactId);
-
-    if (!data) {
-      return res.status(404).json({
-        status: 404,
-        message: "Contact not found"
-      });
+    if (!contact) {
+      res.status(404).json({ message: 'Contact not found!' });
+      return;
     }
-
-    res.json({
+    res.status(200).json({
       status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data
+      message: `Succesfuly found contact with id ${contactId}!`,
+      data: contact,
     });
   });
 
-  app.use((req, res) => {
-    res.status(404).json({
-      message: `${req.url} not found`
-    });
+  app.use('*', (req, res, next) => {
+    res.status(404).json({ message: 'Not found' });
   });
 
-  app.use((error, req, res, next) => {
+  app.use((err, req, res, next) => {
     res.status(500).json({
-      message: error.message,
+      message: 'Something went wrong',
+      error: err.message,
     });
   });
 
-  const port = Number(env("PORT", 3000));
-  app.listen(port, () => console.log(`Server is running on server ${port} port`));
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
