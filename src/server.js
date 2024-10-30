6197ce625cd13f62df1540dcd516594aebaa5cfa
Tanya Kotvytska <tanyakotvitskaya20@gmail.com>
2024-10-30 14:36:11 +0200

    add new code hw3

--- src/server.js
+++ src/server.js
@@ -2,14 +2,19 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from "./services/contacts.js";
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 console.log(PORT);
 
 export const startServer = () => {
   const app = express();
-  app.use(express.json());
+  app.use(express.json({
+    type: ['application/json', 'application/vnd.api+json'],
+    limit: '100kb',
+  }));
 
   app.use(cors());
   app.use(
@@ -20,41 +25,11 @@ export const startServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
+  app.use(contactsRouter);
 
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({ message: 'Contact not found!' });
-      return;
-    }
-    res.status(200).json({
-      status: 200,
-      message: `Succesfuly found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({ message: 'Not found' });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,34 @@ export const getContactById = async (id) => {
   const contact = await ContactCollection.findById(id);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  const contact = await ContactCollection.create(payload);
+  return contact;
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await ContactCollection.findOneAndDelete({
+    _id: contactId,
+  });
+  return contact;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await ContactCollection.findByIdAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
